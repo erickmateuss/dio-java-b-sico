1d03a718dd267ddd5067f73762a3e084ec285750
Erick <erickmateus0510@gmail.com>
2024-05-06 20:39:19 -0300

    update com as anotações em relação a variaveis

--- java-variáveis/src/TiposVariaveis.java
+++ java-variáveis/src/TiposVariaveis.java
@@ -1,5 +1,10 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 20;
+        short ano = 2024;
+        int cep = 556600000;
+        long cpf = 12345689071L; // se começar com zero, talvez tenha que ser outro tipo
+        float pi = 3.14F; // se começar com zero, talvez tenha que ser outro tipo
+        double salario = 1242.70;
     }
 }
