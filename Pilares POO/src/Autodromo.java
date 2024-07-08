8fb770c3ec24bff383d6975b470322cd92c1ca1a
Erick <erickmateus0510@gmail.com>
2024-07-08 15:10:56 -0300

    adição

--- Pilares POO/src/Autodromo.java
+++ Pilares POO/src/Autodromo.java
@@ -1,8 +1,9 @@
 public class Autodromo {
     public static void main(String[] args) {
         Carro jeep = new Carro();
-        
+        Carro G10 = new Carro();
 
         jeep.ligar();
+        G10.ligar();
     }
 }
