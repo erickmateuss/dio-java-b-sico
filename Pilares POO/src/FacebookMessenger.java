f63c61a8340b21381b03ed2f16b410a56767f0a2
Erick <erickmateus0510@gmail.com>
2024-07-09 14:26:10 -0300

    aplicando herança e abstração

--- Pilares POO/src/FacebookMessenger.java
+++ Pilares POO/src/FacebookMessenger.java
@@ -1,12 +1,12 @@
-public class MSNMessenger extends ServicoMensagemInstantanea {
+public class FacebookMessenger extends ServicoMensagemInstantanea {
     private void validarConectadoInternet() {        
         System.out.println("Validando se está conectado a internet");
     }
     public void enviarMensagem() {
-        System.out.println("Enviando mensagem pelo MSN Messenger");
+        System.out.println("Enviando mensagem pelo Facebook Messenger");
     }
     public void receberMensagem() {
-        System.out.println("Recebendo mensagem pelo MSN Messenger ");
+        System.out.println("Recebendo mensagem pelo Facebook Messenger");
     }
     private void salvarHistoricoMensagem() {
         System.out.println("Salvando o histórico da mensagem");
